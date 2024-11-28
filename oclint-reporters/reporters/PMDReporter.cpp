8a4ffdc7656d98222668c076a7b7175233be16a9
gyj <yanjie.guo@kuwo.cn>
2024-11-29 00:10:12 +0800

    fix build err

--- oclint-reporters/reporters/PMDReporter.cpp
+++ oclint-reporters/reporters/PMDReporter.cpp
@@ -78,14 +78,14 @@ private:
     }
 
     void replaceAll(std::string& str, const std::string& from, const std::string& to) {
-+        if(from.empty())
-+            return;
-+        size_t start_pos = 0;
-+        while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-+            str.replace(start_pos, from.length(), to);
-+            start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-+        }
-+    }
+        if(from.empty())
+            return;
+        size_t start_pos = 0;
+        while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+            str.replace(start_pos, from.length(), to);
+            start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+        }
+    }
 
     void validatePath(std::string &path) {
         replaceAll(path, "<", "&lt;");
